07ea52f832ceea4907a5df48f32c0205e6103a42
Rostyslva Paliuha <rostyslavpaliuha@gmail.com>
2019-01-07 17:10:13 +0200

    refactor ChatScene

--- src/main/java/com/rostyslav/home/socketChat/client/ui/MainChatScene.java
+++ src/main/java/com/rostyslav/home/socketChat/client/ui/MainChatScene.java
@@ -23,43 +23,78 @@ import java.util.Arrays;
 
 public class MainChatScene {
 
+    private Color textColor;
+    private Color backagroundColor;
     private Scene mainChatScene;
     private BufferedReader in;
     private PrintWriter out;
     private String userNickName;
+    private TextArea generalMessageWindow;
+    private TextArea usersOnlineStatus;
 
-    public MainChatScene(Color textColor, Color backGroundColor, Stage mainStage, Socket clientSocket, String username) {
-        userNickName = username;
+    public MainChatScene(Color textColor, Color backGroundColor, Stage mainStage, Socket clientSocket, String userNickName) {
+        this.textColor = textColor;
+        this.backagroundColor = backGroundColor;
+        this.userNickName = userNickName;
+        collectChatScene();
+    }
+
+    public Scene getMainChatScene() {
+        launchReader();
+        return mainChatScene;
+    }
+
+    private void initOutPutInput(Socket clientSocket) {
         try {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream());
         } catch (IOException e) {
+            //alert message
         }
-        GridPane container = new GridPane();
-        container.setBackground(new Background(new BackgroundFill(backGroundColor, CornerRadii.EMPTY, Insets.EMPTY)));
-        container.setVgap(5);
-        container.setHgap(5);
+    }
 
-        Label chatLabel = new Label("General Chat:");
-        chatLabel.setFont(Font.font(16));
-        chatLabel.setTextFill(textColor);
-        TextArea generalChatMessageWindow = new TextArea();
-        generalChatMessageWindow.setPrefSize(350, 240);
-        generalChatMessageWindow.setStyle("-fx-background-color: #2B2B2B;");
-        generalChatMessageWindow.setEditable(false);
+    private GridPane initGridPane() {
+        GridPane gridPane = new GridPane();
+        gridPane.setBackground(new Background(new BackgroundFill(backagroundColor, CornerRadii.EMPTY, Insets.EMPTY)));
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+        return gridPane;
+    }
+
+    private VBox initGeneralChatWindow(GridPane gridPane) {
+        Label label = initLabel("General Chat:");
+        generalMessageWindow = initTextArea(350, 240);
         VBox generalChatVBox = new VBox();
-        generalChatVBox.getChildren().addAll(chatLabel, generalChatMessageWindow);
+        generalChatVBox.getChildren().addAll(label, generalMessageWindow);
+        gridPane.add(generalChatVBox, 0, 0);
+        return generalChatVBox;
+    }
 
-        Label usersLabel = new Label("Users online:");
-        usersLabel.setFont(Font.font(16));
-        usersLabel.setTextFill(textColor);
-        TextArea usersTA = new TextArea();
-        usersTA.setPrefSize(150, 240);
-        usersTA.setStyle("-fx-background-color: #2B2B2B;");
-        usersTA.setEditable(false);
+    private VBox initOnlineUsers(GridPane gridPane) {
+        Label label = initLabel("Users online: ");
+        usersOnlineStatus = initTextArea(150, 240);
         VBox usersStatusVB = new VBox();
-        usersStatusVB.getChildren().addAll(usersLabel, usersTA);
+        usersStatusVB.getChildren().addAll(label, usersOnlineStatus);
+        gridPane.add(usersStatusVB, 1, 0);
+        return usersStatusVB;
+    }
 
+    private Label initLabel(String labelName) {
+        Label label = new Label(labelName);
+        label.setFont(Font.font(16));
+        label.setTextFill(textColor);
+        return label;
+    }
+
+    private TextArea initTextArea(int width, int height) {
+        TextArea area = new TextArea();
+        area.setPrefSize(width, height);
+        area.setStyle("-fx-background-color: #2B2B2B;");
+        area.setEditable(false);
+        return area;
+    }
+
+    private HBox initInputConsole(GridPane gridPane) {
         TextField textField = new TextField();
         textField.setPrefWidth(400);
         Button sendButton = new Button("Send");
@@ -67,7 +102,7 @@ public class MainChatScene {
         sendButton.addEventFilter(MouseEvent.MOUSE_CLICKED, event -> {
             String msg = "message:";
             if (textField.getText().length() > 1) {
-                String message = msg.concat(username).concat(" : ").concat(textField.getText());
+                String message = msg.concat(userNickName).concat(" : ").concat(textField.getText());
                 out.println(message);
                 out.flush();
                 textField.clear();
@@ -76,23 +111,23 @@ public class MainChatScene {
         HBox msgSenderVB = new HBox();
         msgSenderVB.setSpacing(5);
         msgSenderVB.getChildren().addAll(textField, sendButton);
+        gridPane.add(msgSenderVB, 0, 2, 2, 1);
+        return msgSenderVB;
+    }
 
-        container.add(generalChatVBox, 0, 0);
-        container.add(usersStatusVB, 1, 0);
-        container.add(msgSenderVB, 0, 2, 2, 1);
-        mainChatScene = new Scene(container, 500, 300);
+    private void launchReader() {
         Runnable reader = () -> {
             while (true) {
                 try {
                     String inputString = in.readLine();
                     if (inputString.startsWith("message:")) {
                         String message = inputString.replace("message:", "");
-                        generalChatMessageWindow.appendText(message + "\n");
+                        generalMessageWindow.appendText(message + "\n");
                     } else if (inputString.startsWith("online:")) {
-                        String list=inputString.replace("online:", "");
+                        String list = inputString.replace("online:", "");
                         String[] arr = list.split(",");
-                        usersTA.clear();
-                        Arrays.stream(arr).forEach(s -> usersTA.appendText(s + "\n"));
+                        usersOnlineStatus.clear();
+                        Arrays.stream(arr).forEach(s -> usersOnlineStatus.appendText(s + "\n"));
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -102,8 +137,11 @@ public class MainChatScene {
         new Thread(reader).start();
     }
 
-    public Scene getMainChatScene() {
-        return mainChatScene;
+    private void collectChatScene() {
+        GridPane gridPane = initGridPane();
+        initGeneralChatWindow(gridPane);
+        initOnlineUsers(gridPane);
+        initInputConsole(gridPane);
+        mainChatScene = new Scene(gridPane, 500, 300);
     }
-
 }
